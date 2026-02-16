a7fbae2c1718f7636daae7a2370b016b9ea9347f
Iran Ledezma <iranledezma99@icloud.com>
2026-02-16 08:22:31 -0600

    primer commit del nuevo portafolio

---
+++ playground/app.vue
@@ -0,0 +1,45 @@
+
+ <template>
+  <div class="main-container"> 
+    <div class="theme-switch" id="theme-toggle">
+      <i class="fa-solid fa-moon"></i>
+    </div>
+
+    <div class="background-shapes">
+      <div class="shape shape-1"></div>
+      <div class="shape shape-2"></div>
+      <div class="shape shape-3"></div>
+    </div>
+
+    <div class="card">
+      <div class="card-header">
+        <div class="profile-img">
+          <i class="fa-solid fa-user-astronaut"></i>
+        </div>
+        <h1>Irán Ledezma</h1>
+        <h2>Ingeniero en TIC & Desarrollador</h2>
+      </div>
+
+      <div class="card-body">
+        <div class="highlight-badge">
+          <i class="fa-solid fa-certificate"></i>
+          <span>Diplomado en Ciencia de Datos!!</span>
+        </div>
+        <p class="bio">
+          Especialista en desarrollo web y análisis de datos.
+        </p>
+        </div>
+
+      <div class="card-footer">
+        <button class="btn-contact">Contactar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+
+<script setup>
+</script>
